8b2a0a2619114e3cbe572b594dde40728ef74bc0
egriuh <sophia@kuhnlein.com>
2023-12-01 13:50:49 -0800

    Animations + Camera movement added

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,10 +7,12 @@ public class PlayerScript : MonoBehaviour
 {
     // Variables 
     private Rigidbody playerRb;
+    public Animator animator;
 
     public bool isOnGround = true;
 
     public float speed;
+    public float turnSpeed;
     public float horizontalInput;
     public float verticalInput;
     public float jumpForce;
@@ -19,18 +21,20 @@ public class PlayerScript : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
 
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
+        animator.SetFloat("verticalInput", Mathf.Abs(verticalInput));
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
